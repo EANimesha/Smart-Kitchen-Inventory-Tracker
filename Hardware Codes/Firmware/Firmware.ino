2f800bea3af2a2276c78f371bafdb0c08ba81bff
EANimesha <nimeshadilini999@gmail.com>
2020-06-15 03:19:20 +0530

    added arduino code to connect to wifi and send sensor data to firebase database;

--- Hardware Codes/Firmware/Firmware.ino
+++ Hardware Codes/Firmware/Firmware.ino
@@ -3,6 +3,14 @@
 #include "Arduino.h"
 #include "FSR.h"
 
+//Connecting to wifi
+#include <WiFi.h>
+#include <FirebaseESP32.h>
+
+FirebaseData firebaseData;
+
+const char* ssid = "Nimesha";
+const char* password = "11111111";
 
 // Pin Definitions
 #define FSR_1_PIN_1	12
@@ -31,7 +39,22 @@ void setup()
     Serial.begin(9600);
     while (!Serial) ; // wait for serial port to connect. Needed for native USB
     Serial.println("start");
-    
+
+    //Connect to wifi
+    WiFi.begin(ssid, password);
+ 
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+ 
+    Serial.println(".......");
+    Serial.println("WiFi Connected....IP Address:");
+    Serial.println(WiFi.localIP());
+
+    //connect to firebase project
+    Firebase.begin("https://smart-kitchen-18f3f.firebaseio.com/", "AIzaSyC2L6b-K2QQB64H_7k2fnwuLEMDgHgPIDk");
+    Serial.println("Firebase connected");
     
 }
 
@@ -39,13 +62,19 @@ void loop()
 {
     
     float fsr_1Force = fsr_1.getForce();
-    Serial.print(F("Force: ")); Serial.print(fsr_1Force); Serial.println(F(" [g]"));
+    if (Firebase.setDouble(firebaseData, "/user001/item1", fsr_1Force)){
+      Serial.print(F("Force: ")); Serial.print(fsr_1Force); Serial.println(F(" [g]")); 
+    }
 
     float fsr_2Force = fsr_2.getForce();
-    Serial.print(F("Force: ")); Serial.print(fsr_2Force); Serial.println(F(" [g]"));
+    if (Firebase.setDouble(firebaseData, "/user001/item2", fsr_2Force)){
+      Serial.print(F("Force: ")); Serial.print(fsr_2Force); Serial.println(F(" [g]"));
+    }
 
     float fsr_3Force = fsr_3.getForce();
+    if (Firebase.setDouble(firebaseData, "/user001/item3", fsr_3Force)){
     Serial.print(F("Force: ")); Serial.print(fsr_3Force); Serial.println(F(" [g]"));
+    }
 
     delay(5000);
     
